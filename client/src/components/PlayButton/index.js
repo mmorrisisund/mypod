2dc930ce66e2b24346ba51cf85b0ee270b2e45f0
MiMo <michael.a.morris.public@gmail.com>
2020-08-29 18:16:11 -0500

    move react-icon styling into component

--- client/src/components/PlayButton/index.js
+++ client/src/components/PlayButton/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { MdPlayCircleFilled, MdPauseCircleFilled } from 'react-icons/md'
+import { IconContext } from 'react-icons'
+
 import style from './style.module.css'
 
-export const PlayButton = ({ onClick, disabled, isPlaying, ...otherProps }) => {
+export const PlayButton = ({ onClick, disabled, isPlaying }) => {
   const disabledColor = disabled ? 'var(--mainGrey)' : 'var(--primaryColor)'
+  const iconStyle = { style: { width: '100%', height: '100%' } }
 
   return (
-    <button
-      className={style.controlButtonPrimary}
-      onClick={onClick}
-      disabled={disabled}
-      style={{ color: disabledColor }}
-    >
-      {isPlaying ? <MdPauseCircleFilled /> : <MdPlayCircleFilled />}
-    </button>
+    <IconContext.Provider value={iconStyle}>
+      <button
+        className={style.controlButtonPrimary}
+        onClick={onClick}
+        disabled={disabled}
+        style={{ color: disabledColor }}
+      >
+        {isPlaying ? <MdPauseCircleFilled /> : <MdPlayCircleFilled />}
+      </button>
+    </IconContext.Provider>
   )
 }
