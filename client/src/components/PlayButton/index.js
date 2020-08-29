92739c8e4b3aea11eb3082626afb8e800987601a
MiMo <michael.a.morris.public@gmail.com>
2020-08-29 18:31:12 -0500

    add ability to apply additional styling

--- client/src/components/PlayButton/index.js
+++ client/src/components/PlayButton/index.js
@@ -2,19 +2,26 @@ import React from 'react'
 import { MdPlayCircleFilled, MdPauseCircleFilled } from 'react-icons/md'
 import { IconContext } from 'react-icons'
 
-import style from './style.module.css'
+import css from './style.module.css'
 
-export const PlayButton = ({ onClick, disabled, isPlaying }) => {
+export const PlayButton = ({
+  onClick,
+  disabled,
+  isPlaying,
+  style,
+  ...otherProps
+}) => {
   const disabledColor = disabled ? 'var(--mainGrey)' : 'var(--primaryColor)'
   const iconStyle = { style: { width: '100%', height: '100%' } }
 
   return (
     <IconContext.Provider value={iconStyle}>
       <button
-        className={style.controlButtonPrimary}
+        className={css.controlButtonPrimary}
         onClick={onClick}
         disabled={disabled}
-        style={{ color: disabledColor }}
+        style={{ color: disabledColor, ...style }}
+        {...otherProps}
       >
         {isPlaying ? <MdPauseCircleFilled /> : <MdPlayCircleFilled />}
       </button>
