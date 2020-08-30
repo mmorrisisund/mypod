b74dc5121ea9634604c4caf1db8c1d1d85230197
MiMo <michael.a.morris.public@gmail.com>
2020-08-30 16:32:00 -0500

    move styling to separte css file

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,47 +1,20 @@
 import React, { useState } from 'react'
 
+import style from './style.module.css'
+
 export const SearchForm = ({ onSearch }) => {
   const [term, setTerm] = useState('dollop')
 
   return (
-    <div
-      style={{
-        backgroundColor: 'var(--mainWhite)',
-        maxWidth: '75%',
-        padding: '3rem',
-        borderRadius: 5,
-        boxShadow: 'var(--lightShadow)',
-        margin: '1rem',
-        display: 'flex',
-        alignItems: 'center'
-      }}
-    >
+    <div className={style.searchForm}>
       <input
+        className={style.searchBox}
         type='text'
         placeholder='Enter a search term...'
         value={term}
         onChange={({ target }) => setTerm(target.value)}
-        style={{
-          padding: '1rem',
-          border: '1px solid var(--darkGrey)',
-          height: 48,
-          fontSize: '1.5rem'
-        }}
       />
-      <button
-        style={{
-          border: 'none',
-          padding: '1rem',
-          backgroundColor: 'var(--primaryColor)',
-          color: 'var(--mainWhite)',
-          borderTopRightRadius: 5,
-          borderBottomRightRadius: 5,
-          letterSpacing: '1.5px',
-          textTransform: 'uppercase',
-          height: 48
-        }}
-        onClick={() => onSearch(term)}
-      >
+      <button className={style.searchButton} onClick={() => onSearch(term)}>
         Search
       </button>
     </div>
