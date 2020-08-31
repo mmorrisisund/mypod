6f39692184123f61ec90e35613dbf2ace854229f
MiMo <michael.a.morris.public@gmail.com>
2020-08-30 23:17:28 -0500

    add navbar header

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import { Search, PodcastDetails } from './pages'
 import { PlayerBar } from './components/PlayerBar'
+import { NavBar } from './components/NavBar'
 
 function App () {
   return (
     <div>
+      <NavBar />
       <Switch>
         <Route path='/search'>
           <Search />
