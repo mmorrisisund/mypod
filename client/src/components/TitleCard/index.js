4feff48bd2ae245ef389ebe6abcdf0c581097be0
MiMo <michael.a.morris.public@gmail.com>
2020-08-31 22:42:50 -0500

    add subscription button

--- client/src/components/TitleCard/index.js
+++ client/src/components/TitleCard/index.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { MdAdd, MdClear } from 'react-icons/md'
+import { IconContext } from 'react-icons'
 
 import style from './style.module.css'
+import { UserContext } from '../../context/UserContext'
 
 export const TitleCard = ({ podcast }) => {
+  const { subscriptions, addSubscription, removeSubscription } = useContext(
+    UserContext
+  )
+
+  const isSubscribed = () =>
+    subscriptions.find(
+      subscription => subscription.collectionId === podcast.collectionId
+    ) !== undefined
+
+  const handleOnClick = () => {
+    isSubscribed() ? removeSubscription(podcast) : addSubscription(podcast)
+  }
+
   return (
     <div className={style.titleCard}>
       <img
@@ -13,6 +29,23 @@ export const TitleCard = ({ podcast }) => {
       <div className={style.titleCardText}>
         <h1 className={style.titleCardHeading}>{podcast.collectionName}</h1>
         <hr />
+        <button className={style.subscribeButton} onClick={handleOnClick}>
+          <IconContext.Provider value={{ style: { height: 16, width: 16 } }}>
+            <span className={style.subscribeButtonContent}>
+              {isSubscribed() ? (
+                <>
+                  <MdClear />
+                  unsubscribe
+                </>
+              ) : (
+                <>
+                  <MdAdd />
+                  subscribe
+                </>
+              )}
+            </span>
+          </IconContext.Provider>
+        </button>
       </div>
     </div>
   )
