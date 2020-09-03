a6b9b9da2a7912446497c99e4ff7c30a30c82337
MiMo <michael.a.morris.public@gmail.com>
2020-09-02 22:53:48 -0500

    use jsonp to get around cors

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import jsonpAdapter from 'axios-jsonp'
 
 import { searchUrl } from '../util/url'
 import { SearchForm } from '../components/SearchForm'
@@ -9,7 +10,10 @@ export const Search = () => {
   const [searchResults, setSearchResults] = useState(undefined)
 
   const handleOnSearch = async term => {
-    const { data } = await axios.get(`${searchUrl}${term}`)
+    const { data } = await axios({
+      url: `${searchUrl}${term}`,
+      adapter: jsonpAdapter
+    })
     setSearchResults(data.results)
   }
 
