2ab9661c38a2933ab79b3c5b92e0dd14e111137a
MiMo <michael.a.morris.public@gmail.com>
2020-09-06 01:27:14 -0500

    add proxy for netlify function during development

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function (app) {
+  app.use(
+    '/.netlify/functions',
+    createProxyMiddleware({
+      target: 'http://localhost:9000',
+      changeOrigin: true,
+      pathRewrite: { '^/\\.netlify/functions': '' }
+    })
+  )
+}
