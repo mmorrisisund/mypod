05845da26a0cbf97814f7a78c0c4c80873bbc0c4
MiMo <michael.a.morris.public@gmail.com>
2020-09-06 00:07:34 -0500

    add 404 handler and fix parameter destructure

--- src/api/api.js
+++ src/api/api.js
@@ -7,12 +7,13 @@ const app = express()
 
 app.get('/api/v1/search', searchHandler)
 app.get('/api/v1/podcast/:id', lookupHandler)
+app.get('*', notFoundHandler)
 
 module.exports.handler = serverless(app)
 
 async function searchHandler (req, res) {
   const searchUrl = 'https://itunes.apple.com/search?media=podcast&term='
-  const { term } = req.body.query
+  const { term } = req.query
 
   try {
     const { data } = await axios.get(`${searchUrl}${term}`)
@@ -24,18 +25,22 @@ async function searchHandler (req, res) {
 
 async function lookupHandler (req, res) {
   const lookupUrl = 'https://itunes.apple.com/lookup?id='
-  const { id } = req.body.params
+  const { id } = req.params
   const parser = new Parser()
 
   try {
     const { data: itunesResults } = await axios.get(`${lookupUrl}${id}`)
-    const itunesInfo = itunesResults[0]
+    const itunesInfo = itunesResults.results[0]
 
     const { data: rawRss } = await axios.get(itunesInfo.feedUrl)
-    const rssFeed = parser.parseString(rawRss)
+    const rssFeed = await parser.parseString(rawRss)
 
     res.status(200).json({ status: 'success', data: { itunesInfo, rssFeed } })
   } catch (error) {
     res.status(400).json({ status: 'error', message: error.message })
   }
+}
+
+function notFoundHandler (req, res) {
+  res.status(404).json({ status: 'error', message: 'destination not found' })
 }
